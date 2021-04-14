4792faa087f20541d0804869fa11d6bb69b9cfc7
fireFerry <info@fireferry.tech>
2021-04-14 19:32:14 +0200

    Added new commands

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ async def support(ctx):
 # port forwarding command
 
 
-@bot.command()
+@bot.command(aliases=['portforward', 'pfw', ''])
 async def portforwarding(ctx):
     embed = discord.Embed(title="Port forwarding",
                           description="Port forwarding is done on your router, and may be called port mapping, or virtual servers too. Port triggering is not the same as port forwarding. \nTo use the web-gui version of byob you need to forward ports 1337-1339 to your machine that you're hosing byob on.",
@@ -223,7 +223,7 @@ async def portforwarding(ctx):
 # requirements command
 
 
-@bot.command()
+@bot.command(aliases=['req'])
 async def requirements(ctx):
     embed = discord.Embed(title="Requirements", description="requirements for byob:", color=0x5cffb0)
     embed.add_field(name="OS", value="A Linux distribution", inline=False)
@@ -251,6 +251,30 @@ async def wsl(ctx):
 async def vps(ctx):
     embed = discord.Embed(title="Virtual Private Server",
                           description="Byob is not recommended on a vps. If you are using a vps for byob you may need to do some extra configuration with your vps provider. You also need to be able to open ports if you want to use byob, staff will not help with this.",
+                          color=0x5cffb0)
+    await ctx.message.delete()
+    await ctx.send(embed=embed)
+
+
+# executable command
+
+
+@bot.command(aliases=['exe'])
+async def executable(ctx):
+    embed = discord.Embed(title="Executable generation",
+                          description="If your executable doesn't generate correctly, here are some things you should check:\n**1.**Make sure you are using the latest version of byob and rebooted at least once.\n**2.**Run this command: sudo usermod -aG docker $USER && sudo chmod 666 /var/run/docker.sock, and reboot your system.\n**3.**If this still doesn't work, uninstall docker, and run startup.sh again, and reboot your system.\n**4.**If you tried all of this and it didn't help, you can try using pyinstaller to compile the python payload manually.",
+                          color=0x5cffb0)
+    await ctx.message.delete()
+    await ctx.send(embed=embed)
+
+
+# wiki command
+
+
+@bot.command()
+async def wiki(ctx):
+    embed = discord.Embed(title="Wiki",
+                          description="web-gui wiki: https://byob.dev/guide\ncli wiki: https://github.com/malwaredllc/byob/wiki\nGitHub: https://github.com/malwaredllc/byob/",
                           color=0x5cffb0)
     await ctx.message.delete()
     await ctx.send(embed=embed)
