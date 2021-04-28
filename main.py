126aa0314862890177b0f348fbd30ed0d16c7af2
fireFerry <info@fireferry.tech>
2021-04-28 14:27:44 +0200

    Version number update. Ready to be released.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from datetime import timedelta, datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-byob_bot_version = '1.2.6.1'
+byob_bot_version = '1.2.6.2'
 intents = discord.Intents.default()
 intents.members = True
 
