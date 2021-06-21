9a22d4cdc2f71bf9fa37ebe822a6ce0d16ad59fc
fireFerry <info@fireferry.tech>
2021-06-21 16:31:11 +0200

    Small debugging test

--- main.py
+++ main.py
@@ -201,6 +201,7 @@ async def on_message(message):
         user = message.author
         guild_id = 817532239783919637
         support_server = bot.get_guild(guild_id)
+        print(support_server.id)
         member = await support_server.fetch_member(user.id)
 
         match = False
