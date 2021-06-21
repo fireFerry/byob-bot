54088bfda6b95ab226e3eb2a4f04313d59e31863
fireFerry <info@fireferry.tech>
2021-06-21 14:53:45 +0200

    Slight change to resolve error

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ async def on_message(message):
         user = message.author
         guild_id = 817532239783919637
         support_server = bot.get_guild(guild_id)
-        member = support_server.get_member(user.id)
+        member = support_server.fetch_member(user.id)
 
         match = False
 
