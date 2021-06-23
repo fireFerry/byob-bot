1c3af9400e92890d53d82310e4e6c8528072837f
fireFerry <info@fireferry.tech>
2021-06-23 16:06:59 +0200

    Added a nice embedded message for the ticket transcript.

--- main.py
+++ main.py
@@ -889,7 +889,7 @@ async def close(ctx):
             transcript_channel = discord.utils.get(ctx.guild.text_channels, name="ticket-transcripts")
             embed2 = discord.Embed(color=0x5cffb0)
             embed2.set_author(name=f"{send_member.name}{send_member.discriminator}",
-                             icon_url=f"{send_member.avatar_url}")
+                              icon_url=f"{send_member.avatar_url}")
             embed2.add_field(name="**Ticket ", value=f"{send_member.mention}", inline=True)
             embed2.add_field(name="**Ticket Owner ID**", value=f"{send_member.id}", inline=True)
             embed2.add_field(name="**Ticket Name**", value=f"{ctx.channel.name}", inline=True)
