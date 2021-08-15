9b0543fca149b241b455771a79b3685ae7c4b111
fireFerry <info@fireferry.tech>
2021-08-15 11:20:26 +0200

    Fixed ticket closing when user left the server.

--- main.py
+++ main.py
@@ -209,7 +209,31 @@ async def on_message(message):
             ctx = await bot.get_context(message)
             user_id = message.channel.name
             user_id = user_id.split("-")[1]
-            send_member = await commands.MemberConverter().convert(ctx, user_id)
+            send_guild = bot.get_guild(guild_id)
+            if send_guild.get_member(user_id) is not None:
+                send_member = await commands.MemberConverter().convert(ctx, user_id)
+            else:
+                send_member = await commands.UserConverter().convert(ctx, user_id)
+                embed = discord.Embed(title="Ticket closed because user left the server", description="Ticket will be deleted in 5 seconds...",
+                                      color=0xaa5858)
+                await message.channel.send(embed=embed)
+                transcript = await chat_exporter.export(message.channel)
+                if transcript is None:
+                    return
+                transcript_file = discord.File(io.BytesIO(transcript.encode()),
+                                               filename=f"transcript-{message.channel.name}.html")
+                transcript_channel = discord.utils.get(message.guild.text_channels, name="ticket-transcripts")
+                embed = discord.Embed(color=0x5cffb0)
+                embed.set_author(name=f"{send_member.name}#{send_member.discriminator}",
+                                 icon_url=f"{send_member.avatar_url}")
+                embed.add_field(name="**Ticket Owner**", value=f"{send_member.mention}", inline=True)
+                embed.add_field(name="**Ticket Owner ID**", value=f"{send_member.id}", inline=True)
+                embed.add_field(name="**Ticket Name**", value=f"{message.channel.name}", inline=True)
+                await transcript_channel.send(embed=embed, file=transcript_file)
+                await asyncio.sleep(5)
+                await message.channel.delete(reason="Ticket closed.")
+                return
+
             dm_channel = await send_member.create_dm()
             if message.guild.id == guild_id:
                 if str(message.attachments) != "[]":
@@ -318,12 +342,17 @@ async def on_button_click(interaction):
         ticket_channel = interaction.channel
         user_id = ticket_channel.name.split("-")[1]
         ctx = await bot.get_context(interaction.message)
-        send_member = await commands.MemberConverter().convert(ctx, user_id)
-        dm_channel = await send_member.create_dm()
-        embed = discord.Embed(title="Ticket Closed",
-                              description="A staff member has closed your ticket. Sending a new message will create a new ticket, please only do so if you have another issue.",
-                              color=0xc9cb65)
-        await dm_channel.send(embed=embed)
+        send_guild = bot.get_guild(guild_id)
+        if send_guild.get_member(user_id) is not None:
+            send_member = await commands.MemberConverter().convert(ctx, user_id)
+            dm_channel = await send_member.create_dm()
+            embed = discord.Embed(title="Ticket Closed",
+                                  description="A staff member has closed your ticket. Sending a new message will create a new ticket, please only do so if you have another issue.",
+                                  color=0xc9cb65)
+            await dm_channel.send(embed=embed)
+        else:
+            send_member = await commands.UserConverter().convert(ctx, user_id)
+
         embed = discord.Embed(title="Ticket closed",
                               description="Ticket will be deleted in 5 seconds...",
                               color=0xaa5858)
@@ -926,12 +955,17 @@ async def close(ctx):
             await ctx.message.delete()
             ticket_channel = ctx.channel
             user_id = ctx.channel.name.split("-")[1]
-            send_member = await commands.MemberConverter().convert(ctx, user_id)
-            dm_channel = await send_member.create_dm()
-            embed = discord.Embed(title="Ticket Closed",
-                                  description="A staff member has closed your ticket. Sending a new message will create a new ticket, please only do so if you have another issue.",
-                                  color=0xc9cb65)
-            await dm_channel.send(embed=embed)
+            send_guild = bot.get_guild(guild_id)
+            if send_guild.get_member(user_id) is not None:
+                send_member = await commands.MemberConverter().convert(ctx, user_id)
+                dm_channel = await send_member.create_dm()
+                embed = discord.Embed(title="Ticket Closed",
+                                      description="A staff member has closed your ticket. Sending a new message will create a new ticket, please only do so if you have another issue.",
+                                      color=0xc9cb65)
+                await dm_channel.send(embed=embed)
+            else:
+                send_member = await commands.UserConverter().convert(ctx, user_id)
+
             embed = discord.Embed(title="Ticket closed", description="Ticket will be deleted in 5 seconds...",
                                   color=0xaa5858)
             await ticket_channel.send(embed=embed)
