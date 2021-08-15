2de240165233bb904fa6539f366c246040694fef
fireFerry <info@fireferry.tech>
2021-08-15 11:49:48 +0200

    Added some debugging prints

--- main.py
+++ main.py
@@ -211,7 +211,6 @@ async def on_message(message):
             user_id = user_id.split("-")[1]
             send_guild = bot.get_guild(guild_id)
             print("1")
-            print(send_guild.get_member(user_id).name)
             print(send_guild.get_member(user_id))
             if send_guild.get_member(user_id) is not None:
                 send_member = await commands.MemberConverter().convert(ctx, user_id)
