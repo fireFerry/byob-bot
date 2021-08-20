a3500bfb3ae0cd78868eb63536f5ffb01d6d4206
fireFerry <info@fireferry.tech>
2021-08-20 11:10:18 +0200

    Added debugging #2

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ async def on_command_error(_, error):
 
 @bot.event
 async def on_message(message):
-    if hasattr(message.channel, 'category') and str(message.channel.category) == "Active Tickets" and message.author != bot.user:
+    if hasattr(message.channel, 'category') and str(message.channel.category) == "Active Tickets" and not message.author.bot:
         print('yes')
         ctx = await bot.get_context(message)
         user_id = message.channel.name
